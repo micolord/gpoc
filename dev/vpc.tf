e24ab995534e6552f64ced2b21360be2582d1b40
Michael Angelo Viola <mico@Michaels-MacBook-Pro.local>
2023-10-10 18:06:57 +0800

    updated vpc.tf

--- dev/vpc.tf
+++ dev/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "${var.env_name}-${var.project}-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr
 
   azs             = [var.availability_zone_1, var.availability_zone_2]
   private_subnets = [var.private_subnet_cidr_1, var.private_subnet_cidr_2, var.private_subnet_cidr_3]
