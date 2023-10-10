5114eff027f6d24b7263eabd4bc9e7ed67018ace
Michael Angelo Viola <mico@Michaels-MacBook-Pro.local>
2023-10-10 16:32:57 +0800

    removed s3.tf

--- dev/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "${var.env_name}-${var.project}-tfstate"
-
-  tags = {
-    Name        = "${var.env_name}-${var.project}-tfstate"
-    Environment = var.env_name
-  }
-}
