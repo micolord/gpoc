d7c193fe88468e88d73c54fb081d9471d4ba5447
mico <mico.viola@myecloud.net>
2023-10-16 14:35:46 +0800

    Add Files

--- ec2.tf
+++ ec2.tf
@@ -2,20 +2,11 @@ module "gpoc_environment" {
   source  = "github.com/micolord/gpoc-module.git?ref=v1.0.0"
 
   name = "${var.env_name}-${var.project}-GO"
-
-  instance_type          = var.ec2_instance_type
-  ami                    = var.ec2_ami_id
-  key_name               = "user1"
-  monitoring             = true
-  user_data              = file("userdata.sh")
-  associate_public_ip_address = true
-  iam_instance_profile   = "MySessionManagerRole"
-  vpc_security_group_ids = [module.GO_sg.security_group_id]
+  vpc_id      = "vpc-015d0d851ac305f9d"
+  ec2_instance_type          = var.ec2_instance_type
+  ec2_ami_id                    = var.ec2_ami_id
   subnet_id              = var.public_subnet_id_1
-
-  tags = {
-    Environment = var.env_name
-    Project = var.project
-
+  env_name = var.env_name
+  project = var.project
+  
   }
-}
