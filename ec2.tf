79a4ea0aa829c9d71fe738e728a9a679635f805b
mico <mico.viola@myecloud.net>
2023-10-16 14:59:08 +0800

    Add Files

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 module "gpoc_environment" {
   source  = "github.com/micolord/gpoc-module.git?ref=v1.0.0"
 
-  vpc_id      = "vpc-015d0d851ac305f9d"
+  vpc_id      = var.vpc_id
   ec2_instance_type          = var.ec2_instance_type
   ec2_ami_id                    = var.ec2_ami_id
   public_subnet_id_1              = var.public_subnet_id_1
